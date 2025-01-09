ea16643cda9e193023364d2592b0ffff085c2c6e
Jeremiah <jeremiahoclark@gmail.com>
2025-01-09 09:56:52 +0800

    checkpoint

--- app/lib/airtable.ts
+++ app/lib/airtable.ts
@@ -1,10 +1,10 @@
 import Airtable from 'airtable';
 
-if (!process.env.AIRTABLE_API_KEY) {
-  throw new Error('AIRTABLE_API_KEY is required');
+if (!process.env.NEXT_PUBLIC_AIRTABLE_API_KEY) {
+  throw new Error('NEXT_PUBLIC_AIRTABLE_API_KEY is required');
 }
 
-const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base('appmePGTRHHESvrPU');
+const base = new Airtable({ apiKey: process.env.NEXT_PUBLIC_AIRTABLE_API_KEY }).base('appmePGTRHHESvrPU');
 
 export const goalsTable = base('Goals');
 export const progressTable = base('Daily Progress'); 
